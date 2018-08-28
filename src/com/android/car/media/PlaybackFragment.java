unisoc-android/android_packages_apps_Car_Media

Move car media to androidx
Bug: 77333303
Test: local deplyment and test
Change-Id: I04e48eb139fcfc9e38058654ee2daa2cb062a8a6

src/com/android/car/media/PlaybackFragment.java
@@ -19,10 +19,6 @@ package com.android.car.media;
 import android.graphics.Bitmap;
 import android.graphics.PorterDuff;
 import android.os.Bundle;
-import android.os.Handler;
-import android.support.v4.app.Fragment;
-import android.support.v7.widget.GridLayoutManager;
-import android.support.v7.widget.RecyclerView;
 import android.util.Log;
 import android.util.TypedValue;
 import android.view.LayoutInflater;
@@ -31,6 +27,11 @@ import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.SeekBar;
 import android.widget.TextView;
+
+import androidx.car.widget.PagedListView;
+import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import com.android.car.media.browse.BrowseAdapter;
 import com.android.car.media.browse.ContentForwardStrategy;
@@ -45,8 +46,6 @@ import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.Locale;
 import java.util.Objects;
-
-import androidx.car.widget.PagedListView;
 
 /**
  * A {@link Fragment} that implements both the playback and the content forward browsing experience.

nxp-imx-android/aosp_platform_packages_apps_Car_Media

Merged-In: I04e48eb139fcfc9e38058654ee2daa2cb062a8a6
(cherry picked from commit 3ba5dbe22222f1aa9d4a4da7274d1be2af1a9577)
(cherry picked from commit f9f3dee14778bb7b932f7241b92d91ac1d938c66)
(cherry picked from commit 4b19e7bbdc1573225a4940abdf777ecbc7edb54c)

src/com/android/car/media/PlaybackFragment.java
@@ -16,14 +16,10 @@
 
 package com.android.car.media;
 
+import android.annotation.NonNull;
 import android.content.Context;
 import android.media.session.MediaController;
 import android.os.Bundle;
-import android.support.annotation.NonNull;
-import android.support.constraint.ConstraintLayout;
-import android.support.constraint.ConstraintSet;
-import android.support.v4.app.Fragment;
-import android.support.v7.widget.RecyclerView;
 import android.transition.Transition;
 import android.transition.TransitionInflater;
 import android.transition.TransitionListenerAdapter;
@@ -35,6 +31,16 @@ import android.widget.ImageView;
 import android.widget.SeekBar;
 import android.widget.TextView;
 
+import androidx.car.widget.ListItem;
+import androidx.car.widget.ListItemAdapter;
+import androidx.car.widget.ListItemProvider;
+import androidx.car.widget.PagedListView;
+import androidx.car.widget.TextListItem;
+import androidx.constraintlayout.widget.ConstraintLayout;
+import androidx.constraintlayout.widget.ConstraintSet;
+import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.RecyclerView;
+
 import com.android.car.media.common.MediaItemMetadata;
 import com.android.car.media.common.MediaSource;
 import com.android.car.media.common.PlaybackControls;
@@ -43,12 +49,6 @@ import com.android.car.media.common.PlaybackModel;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
-
-import androidx.car.widget.ListItem;
-import androidx.car.widget.ListItemAdapter;
-import androidx.car.widget.ListItemProvider;
-import androidx.car.widget.PagedListView;
-import androidx.car.widget.TextListItem;
 
 /**
  * A {@link Fragment} that implements both the playback and the content forward browsing experience.